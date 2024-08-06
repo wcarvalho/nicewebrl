b1973e93ef8e0573a2cfdb4db3d5ec89498faf39
Wilka Carvalho <wcarvalho92@gmail.com>
2024-08-06 00:23:56 -0400

    updated readme

--- README.md
+++ README.md
@@ -12,3 +12,62 @@ conda create -n nicewebrl python=3.10 pip wheel -y
 conda activate nicewebrl
 pip install -r requirements.tx
 ```
+
+
+### Jax environments
+
+<table style="width:100%; border-collapse: collapse;">
+  <tr>
+    <td style="border: 1px solid black; padding: 10px; text-align: center;">
+      <a href="https://github.com/MichaelTMatthews/Craftax" target="_blank" style="text-decoration: none; color: inherit;">
+        <strong>Craftax</strong>
+      </a><br>
+      <a href="https://github.com/MichaelTMatthews/Craftax" target="_blank">
+        <img src="https://raw.githubusercontent.com/MichaelTMatthews/Craftax/main/images/building.gif" alt="Craftax" style="width: 100%; max-width: 400px;">
+      </a>
+    </td>
+    <td style="border: 1px solid black; padding: 10px; text-align: center;">
+      <a href="https://github.com/wcarvalho/JaxHouseMaze" target="_blank" style="text-decoration: none; color: inherit;">
+        <strong>Housemaze</strong>
+      </a><br>
+      <a href="https://github.com/wcarvalho/JaxHouseMaze" target="_blank">
+        <img src="https://github.com/wcarvalho/JaxHouseMaze/raw/main/example.png" alt="Housemaze" style="width: 100%; max-width: 400px;">
+      </a>
+    </td>
+    <td style="border: 1px solid black; padding: 10px; text-align: center;">
+      <a href="https://github.com/corl-team/xland-minigrid" target="_blank" style="text-decoration: none; color: inherit;">
+        <strong>XLand-Minigrid</strong>
+      </a><br>
+      <a href="https://github.com/corl-team/xland-minigrid" target="_blank">
+        <img src="https://github.com/corl-team/xland-minigrid/blob/main/figures/ruleset-example.jpg?raw=true" alt="XLand-Minigrid" style="width: 100%; max-width: 400px;">
+      </a>
+    </td>
+  </tr>
+  <tr>
+    <td style="border: 1px solid black; padding: 10px; text-align: center;">
+      <a href="https://github.com/epignatelli/navix" target="_blank" style="text-decoration: none; color: inherit;">
+        <strong>Navix (Jax minigrid)</strong>
+      </a><br>
+      <a href="https://github.com/epignatelli/navix" target="_blank">
+        <img src="https://minigrid.farama.org/_images/GoToObjectEnv.gif" alt="Navix" style="width: 100%; max-width: 400px;">
+      </a>
+    </td>
+    <td style="border: 1px solid black; padding: 10px; text-align: center;">
+      <a href="https://github.com/FLAIROx/JaxMARL" target="_blank" style="text-decoration: none; color: inherit;">
+        <strong>Overcooked (multi-agent)</strong>
+      </a><br>
+      <a href="https://github.com/FLAIROx/JaxMARL" target="_blank">
+        <img src="https://github.com/FLAIROx/JaxMARL/blob/main/docs/imgs/cramped_room.gif?raw=true" alt="Overcooked" style="width: 100%; max-width: 400px;">
+      </a>
+    </td>
+    <td style="border: 1px solid black; padding: 10px; text-align: center;">
+      <a href="https://github.com/FLAIROx/JaxMARL" target="_blank" style="text-decoration: none; color: inherit;">
+        <strong>STORM: Spatial-Temporal Matrix Games (multi-agent)</strong>
+      </a><br>
+      <a href="https://github.com/FLAIROx/JaxMARL" target="_blank">
+        <img src="https://github.com/FLAIROx/JaxMARL/blob/main/docs/imgs/storm.gif?raw=true" alt="STORM" style="width: 100%; max-width: 400px;">
+      </a>
+    </td>
+  </tr>
+</table>
+
--- nicewebrl/stages.py
+++ nicewebrl/stages.py
@@ -52,6 +52,33 @@ async def save_stage_state(stage_state):
     )
     await model.save()
 
+
+async def update_stage_on_keypress(e, container, stages):
+    """Default behavior for progressing through stages."""
+    if app.storage.user['stage_idx'] >= len(stages):
+        finish_experiment(container)
+        return 
+
+    stage = stages[app.storage.user['stage_idx']]
+    stage_state = stage.get_user_data('stage_state')
+    stage_finished = stage_state.finished
+    if stage_finished:
+        app.storage.user['stage_idx'] += 1
+    stage_idx = app.storage.user['stage_idx']
+    container.clear()
+    if stage_idx < len(stages):
+        stage = stages[app.storage.user['stage_idx']]
+        await stage.run(container)
+    else:
+        finish_experiment(container)
+
+def finish_experiment(container):
+    app.storage.user['experiment_finished'] = app.storage.user.get('experiment_finished', True)
+    with container:
+        container.clear()
+        ui.markdown(f"# Experiment over. You may close the browser")
+
+
 @struct.dataclass
 class StageState:
     finished: bool = False
@@ -66,7 +93,9 @@ class EnvStageState(StageState):
 @dataclasses.dataclass
 class Stage:
     name: str = 'stage'
+    body: str = 'stage'
     state_cls: StageState = StageState
+    display_fn: Callable = None
 
     def run(self, container: ui.element): pass
 
@@ -86,15 +115,20 @@ class Stage:
 @dataclasses.dataclass
 class EnvStage(Stage):
     instruction: str = 'instruction'
+    max_episodes: Optional[int] = 10
+    min_success: Optional[int] = 1
     web_env: Any = None
     action_to_key: Dict[int, str] = None
     env_params: struct.PyTreeNode = None
     reset_env: bool = True
     render_fn: Callable = None
     multi_render_fn: Callable = None
-    task_desc_fn: Callable = None
     evaluate_success_fn: Callable = lambda t: 0
     state_cls: StageState = None
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.multi_render_fn = jax.jit(jax.vmap(self.render_fn))
 
     def restart_env(self):
         rng = new_rng()
@@ -105,23 +139,6 @@ class EnvStage(Stage):
         return timestep
 
     def send_timestep_to_client(self, container, timestep):
-        image = self.render_fn(timestep)
-        image = base64_nparray(image)
-        with container.style('align-items: center;'):
-            container.clear()
-            ui.markdown(f"# {self.name}")
-            ui.markdown(f"### {self.instruction}")
-            if self.task_desc_fn is not None:
-                self.task_desc_fn(timestep)
-            ui.html(make_image_html(src=image))
-            ui.run_javascript("window.imageSeenTime = new Date();")
-            # ui.html(make_image_html(src=''))
-            # js = f"document.getElementById('stateImage').src={image};"
-            # js += "\nwindow.imageSeenTime = new Date();"
-            # print(js)
-            # for accurate times
-            # ui.run_javascript(js)
-
         #############################
         # get next images and store them client-side
         # setup code to display next wind
@@ -134,32 +151,96 @@ class EnvStage(Stage):
             self.action_to_key[idx]: base64_nparray(image) for idx, image in enumerate(next_images)}
         js_code = f"window.next_states = {next_images};"
         ui.run_javascript(js_code)
-        return next_timesteps
+        self.set_user_data(
+            next_timesteps=next_timesteps,
+        )
+        #############################
+        # display image
+        #############################
+        self.display_fn(
+            stage=self,
+            container=container,
+            timestep=timestep)
+        ui.run_javascript("window.imageSeenTime = new Date();")
 
     async def start_stage(self, container: ui.element):
         timestep = self.restart_env()
-        next_timesteps = self.send_timestep_to_client(container, timestep)
         stage_state = self.state_cls(timestep=timestep)
-        await save_stage_state(stage_state)
         self.set_user_data(
-            timestep=timestep,
-            next_timesteps=next_timesteps,
+            #timestep=timestep,
             stage_state=stage_state,
         )
+        await save_stage_state(stage_state)
+        self.send_timestep_to_client(container, timestep)
 
     def load_stage(self, container: ui.element, stage_state: StageState):
         dummy_timestep = self.restart_env()
         timestep = nicejax.cast_match(
             example=dummy_timestep, data=stage_state.timestep)
-        next_timesteps = self.send_timestep_to_client(container, timestep)
         self.set_user_data(
-            timestep=timestep,
-            next_timesteps=next_timesteps,
-            stage_state=stage_state,
+            stage_state=stage_state.replace(
+                timestep=timestep),
         )
+        self.send_timestep_to_client(container, timestep)
+
+    async def handle_key_press(self, container, e):
+        stage_state = self.get_user_data('stage_state')
+        if stage_state.finished: return
+        key = e.args['key']
+        keydownTime = e.args['keydownTime']
+        imageSeenTime = e.args['imageSeenTime']
+        key_to_action = {k: a for a, k in self.action_to_key.items()}
+        action_idx = key_to_action[key]
+
+        next_timesteps = self.get_user_data('next_timesteps')
+        timestep = jax.tree_map(
+            lambda t: t[action_idx], next_timesteps)
+        self.send_timestep_to_client(container, timestep)
+        success = self.evaluate_success_fn(timestep)
+
+        stage_state = stage_state.replace(
+            timestep=timestep,
+            nsteps=stage_state.nsteps + 1,
+            nepisodes=stage_state.nepisodes + timestep.last(),
+            nsuccesses=stage_state.nsuccesses + success,
+        )
+        await save_stage_state(stage_state)
+        self.set_user_data(
+            #timestep=timestep,
+            #next_timesteps=next_timesteps,
+            stage_state=stage_state,
+            keydownTime=keydownTime,
+            imageSeenTime=imageSeenTime,
+        )
+        ################
+        # Episode over?
+        ################
+        if timestep.last():
+            if success:
+                ui.notify('success',
+                          type='positive',
+                          position='center',
+                          timeout=50,
+                          )
+            else:
+                ui.notify('failure',
+                          type='negative',
+                          position='center',
+                          timeout=50)
+
+        ################
+        # Stage over?
+        ################
+        achieved_min_success = stage_state.nsuccesses >= self.min_success
+        achieved_max_episodes = stage_state.nepisodes >= self.max_episodes
+
+        go_to_next_stage = achieved_min_success or achieved_max_episodes
+        stage_state = stage_state.replace(
+            finished=go_to_next_stage)
+        self.set_user_data(stage_state=stage_state)
+
 
     async def run(self, container: ui.element):
-
         # (potentially) load stage state from memory
         stage_state = await get_latest_stage_state(
             cls=self.state_cls)
@@ -169,32 +250,5 @@ class EnvStage(Stage):
         else:
             self.load_stage(container, stage_state)
 
-        async def handle_key_press(e):
-            key = e.args['key']
-            keydownTime = e.args['keydownTime']
-            imageSeenTime = e.args['imageSeenTime']
-            key_to_action = {k: a for a, k in self.action_to_key.items()}
-            action_idx = key_to_action[key]
-
-            next_timesteps = self.get_user_data('next_timesteps')
-            timestep = jax.tree_map(lambda t: t[action_idx], next_timesteps)
-            next_timesteps = self.send_timestep_to_client(container, timestep)
-            success = self.evaluate_success_fn(timestep)
-
-            stage_state = self.get_user_data('stage_state')
-            stage_state = stage_state.replace(
-                timestep=timestep,
-                nsteps=stage_state.nsteps + 1,
-                nepisodes=stage_state.nepisodes + timestep.last(),
-                nsuccesses=stage_state.nepisodes + success,
-            )
-            await save_stage_state(stage_state)
-            self.set_user_data(
-                timestep=timestep,
-                next_timesteps=next_timesteps,
-                stage_state=stage_state,
-                keydownTime=keydownTime,
-                imageSeenTime=imageSeenTime,
-            )
-
-        ui.on('key_pressed', handle_key_press)
+        ui.on('key_pressed', 
+              lambda e: self.handle_key_press(container, e))
