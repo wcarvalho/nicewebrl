c3a918e8d9202bbe7f36fc624500cef9d9e073e3
Hoon Shin <misterhoonster@gmail.com>
2025-05-14 17:07:35 -0400

    add loading screen + make deployable

--- examples/craftax/experiment_structure.py
+++ examples/craftax/experiment_structure.py
@@ -85,7 +85,7 @@ async def instruction_display_fn(stage, container):
     )
 
 
-instruction_stage = Stage(name="Instuctions", display_fn=instruction_display_fn)
+instruction_stage = Stage(name="Instructions", display_fn=instruction_display_fn)
 all_stages.append(instruction_stage)
 
 
